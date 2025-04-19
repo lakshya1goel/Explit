d8c72058e1d02bbb531cb0ddaadf6622628dbe8d
lakshya1goel <lakshya1234goel@gmail.com>
2025-04-19 11:54:50 +0530

    navigation setup

---
+++ src/navigation/AppNavigator.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { RootStackParamList } from '../types';
+import SignUpScreen from '../screens/auth/SignUp';
+
+const Stack = createStackNavigator<RootStackParamList>();
+
+const AppNavigator = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="SignUp" component={SignUpScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+};
+export default AppNavigator;
---
+++ src/screens/auth/SignUp.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const SignUpScreen = () => {
+    return (
+        <View>
+            <Text>Sign Up</Text>
+        </View>
+    );
+};
+export default SignUpScreen;
